23393dc42a9a1fecb51c26c28e96df64be933475
KatiSteinke <KatiSteinke@users.noreply.github.com>
2018-05-04 15:34:58 +0200

    common.pattern_search: handle N and C termini

--- antismash/common/pattern_search.py
+++ antismash/common/pattern_search.py
@@ -27,6 +27,30 @@ class Match:
     def __bool__(self) -> bool:
         return self.hit
 
+    def __repr__(self) -> str:
+            return str(self)
+
+    def __str__(self) -> str:
+        if not self.hit:
+            return "Match(False)"
+        return "Match(%s, %s)" % (self.hit, self.distance)
+
+
+class MatchLocation:
+    def __init__(self, start: int, end: int) -> None:
+        self.start = start
+        self.end = end
+
+    @property
+    def length(self) -> int:
+        return self.end - self.start
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return "Match from {} to {} (length: {})".format(self.start, self.end, self.length)
+
 
 class Element:
     def __init__(self, min_repeats: int, max_repeats: int, next_element: "Element" = None) -> None:
@@ -34,27 +58,57 @@ class Element:
         self.max_repeats = max_repeats
         self.next_element = next_element
 
-    def match(self, sequence: str) -> Match:
+    def match(self, sequence: str, offset: int = 0) -> Match:
         raise NotImplementedError('Missing match implementation')
 
-    def match_including_following(self, sequence: str, offset: int = 0) -> Match:  # TODO: make findall-ified copy
-        for repeat in range(self.max_repeats, self.min_repeats - 1, -1):  # start with highest number repeats: greedy
+    def match_including_following(self, sequence: str, offset: int = 0) -> Match:
+        repeats = list(range(self.max_repeats, self.min_repeats - 1, -1))
+        for repeat in repeats:  # start with highest number repeats: greedy
             if repeat:
-                if (len(sequence) - offset) < repeat:  # if repeats are longer than remaining sequence...
+                if (len(sequence) - offset) < repeat - 1:  # if repeats are longer than remaining sequence...
                     continue  # ...try the next lowest number of repeats
                 matches = True
                 for idx in range(offset, offset + repeat):
-                    match = self.match(sequence[idx])
+                    match = self.match(sequence, idx)
                     matches = matches and match
                 if not matches:  # if no match was found for the current number of repeats...
                     continue  # ...try the next lowest number of repeats
             if self.next_element:
                 nextmatch = self.next_element.match_including_following(sequence, offset + repeat)
-            else:  # if there is no next element, current element is the end of the "chain", no further search needed
+            else:
                 return Match(True, repeat)
             if nextmatch:  # since Match instances are evaluated as bool here
                 return Match(True, repeat + nextmatch.distance)
         return Match(False)
+
+    def match_all_possible(self, sequence: str, offset: int = 0) -> List[MatchLocation]:
+        results = []
+        repeats = list(range(self.max_repeats, self.min_repeats - 1, -1))
+        for repeat in repeats:  # start with highest number repeats: greedy
+            if repeat:
+                if (len(sequence) - offset) < repeat - 1:  # if repeats are longer than remaining sequence...
+                    continue  # ...try the next lowest number of repeats
+                matches = True
+                for idx in range(offset, offset + repeat):
+                    match = self.match(sequence, idx)
+                    matches = matches and match
+                    if not matches:
+                        break
+                if not matches:
+                    continue
+            if self.next_element:
+                nextmatch = self.next_element.match_all_possible(sequence, offset + repeat)
+                if nextmatch:
+                    newmatch = [MatchLocation(offset, location.end) for location in nextmatch]
+                    results.extend(newmatch)
+            else:
+                if offset + repeat <= len(sequence):
+                    results.append(MatchLocation(offset, offset + repeat))
+                # otherwise covered since it's end terminal that would have the
+                # same coordinates
+        for match in results:
+            assert match.end <= len(sequence), "overlong slice: %s" % match
+        return results
 
 
 class SimpleAmino(Element):
@@ -62,27 +116,66 @@ class SimpleAmino(Element):
         if element[0] not in AMINOS:
             raise ValueError('Invalid amino acid')
         self.amino = element[0]
+        self.optional_c_terminus = False
         min_repeats, max_repeats = parse_repeats(element, 1)
         super().__init__(min_repeats, max_repeats, next_element)
 
-    def match(self, sequence: str) -> Match:
-        if not sequence:
+    def match(self, sequence: str, offset: int = 0) -> Match:
+        if not 0 <= offset < len(sequence):
             return Match(False)
-        return Match(sequence[0] == self.amino, 1)
+        return Match(sequence[offset] == self.amino, 1)
 
     def __str__(self) -> str:
         return "SimpleAmino(%s)(%d,%d)" % (self.amino, self.min_repeats, self.max_repeats)
+
+
+class NTerminalAmino(Element):
+    def __init__(self, element: str, next_element: Element = None) -> None:
+        if element[1] not in AMINOS:
+            raise ValueError('Invalid amino acid')
+        self.amino = element[1]
+        self.optional_c_terminus = False
+        min_repeats, max_repeats = parse_repeats(element, 2)
+        super().__init__(min_repeats, max_repeats, next_element)
+
+    def match(self, sequence: str, offset: int = 0) -> Match:
+        if not 0 <= offset < len(sequence):
+            return Match(False)
+        return Match(sequence.startswith(self.amino) and sequence[offset] == self.amino, 1)
+
+    def __str__(self) -> str:
+        return "NTerminalAmino(%s)(%d,%d)" % (self.amino, self.min_repeats, self.max_repeats)
+
+
+class CTerminalAmino(Element):
+    def __init__(self, element: str, next_element: Element = None) -> None:
+        if element[0] not in AMINOS:
+            raise ValueError('Invalid amino acid')
+        self.amino = element[0]
+        self.optional_c_terminus = False  # weird solution, rename...
+        min_repeats, max_repeats = parse_repeats(element.strip('>'), 1)  # TODO: find actual syntax example
+        super().__init__(min_repeats, max_repeats, next_element)
+
+    def match(self, sequence: str, offset: int = 0) -> Match:
+        if not 0 <= offset < len(sequence):
+            return Match(False)
+        return Match(sequence.endswith(self.amino) and offset == (len(sequence) - 1), 1)
+
+    def __str__(self) -> str:
+        return "CTerminalAmino(%s)(%d,%d)" % (self.amino, self.min_repeats, self.max_repeats)
+
 
 class AnyAmino(Element):
     def __init__(self, element: str, next_element: Element = None) -> None:
         if element[0] != 'x':
             raise ValueError('Attempting to use defined amino acid as AnyAmino')
         min_repeats, max_repeats = parse_repeats(element, 1)
+        self.optional_c_terminus = False
         super().__init__(min_repeats, max_repeats, next_element)
 
-    def match(self, sequence: str) -> Match:
+    def match(self, sequence: str, offset: int = 0) -> Match:
         # TODO: should invalid amino be an error?
-        return Match(len(sequence) > 0, 1)
+        return Match(len(sequence) > offset, 1)
 
     def __str__(self) -> str:
         return "AnyAmino(%d,%d)" % (self.min_repeats, self.max_repeats)
@@ -90,27 +183,27 @@ class AnyAmino(Element):
 
 class MultipleAmino(Element):
     def __init__(self, element: str, next_element: Element = None) -> None:
-        self.options, min_repeats, max_repeats = parse_options(element, "[", "]")
+        self.options, min_repeats, max_repeats, self.optional_c_terminus = parse_options(element, "[", "]")
         super().__init__(min_repeats, max_repeats, next_element)
 
-    def match(self, sequence: str) -> Match:
-        if not sequence:
-            return Match(False)
-        return Match(sequence[0] in self.options, 1)
+    def match(self, sequence: str, offset: int = 0) -> Match:
+        if not 0 <= offset < len(sequence):
+            return Match(self.optional_c_terminus and 0 <= offset <= len(sequence), 0)
+        return Match(sequence[offset] in self.options, 1)
 
     def __str__(self) -> str:
-        return "MultipleAmino(%s)(%d,%d)" % (self.options, self.min_repeats, self.max_repeats)
+        return "MultipleAmino(%s%s)(%d,%d)" % (self.options, ">" if self.optional_c_terminus else "", self.min_repeats, self.max_repeats)
 
 
 class NegatedAmino(Element):
     def __init__(self, element: str, next_element: Element = None) -> None:
-        self.options, min_repeats, max_repeats = parse_options(element, "{", "}")
+        self.options, min_repeats, max_repeats, self.optional_c_terminus = parse_options(element, "{", "}")
         super().__init__(min_repeats, max_repeats, next_element)
 
-    def match(self, sequence: str) -> Match:
-        if not sequence:
+    def match(self, sequence: str, offset: int = 0) -> Match:
+        if not 0 <= offset < len(sequence):
             return Match(False)
-        return Match(sequence[0] not in self.options, 1)
+        return Match(sequence[offset] not in self.options, 1)
 
     def __str__(self) -> str:
         return "MultipleAmino(%s)(%d,%d)" % (self.options, self.min_repeats, self.max_repeats)
@@ -118,9 +211,18 @@ class NegatedAmino(Element):
 
 class Pattern:
     def __init__(self, pattern_string: str) -> None:
+        assert pattern_string.endswith('.')
         self.elements = [None]  # type: List[Element]
-        for part in pattern_string.split('-')[::-1]:
-            if part[0] in AMINOS:
+        self.n_terminus = None  # type: NTerminalAmino
+        self.c_terminus = None  # type: CTerminalAmino
+        for part in pattern_string.strip('.').split('-')[::-1]:
+            if part[-1] == '>':
+                if self.elements[-1] is None and not self.c_terminus:  # only if it's last element
+                    self.c_terminus = CTerminalAmino(part, self.elements[-1])
+                    self.elements.append(self.c_terminus)
+                else:
+                    raise ValueError('Invalid pattern: %s' % pattern_string)
+            elif part[0] in AMINOS:
                 self.elements.append(SimpleAmino(part, self.elements[-1]))
             elif part[0] == 'x':
                 self.elements.append(AnyAmino(part, self.elements[-1]))
@@ -128,44 +230,42 @@ class Pattern:
                 self.elements.append(MultipleAmino(part, self.elements[-1]))
             elif part[0] == '{':
                 self.elements.append(NegatedAmino(part, self.elements[-1]))
+            elif part[0] == '<' and pattern_string.startswith('<') and not self.n_terminus:
+                self.n_terminus = NTerminalAmino(part, self.elements[-1])
+                self.elements.append(self.n_terminus)
             else:
                 raise ValueError('Invalid pattern: %s' % pattern_string)
         self.head = self.elements[-1]
         assert self.head is not None
 
-    def find(self, sequence: str, idx: int = 0) -> int:
-        anchor_idx = self.find_anchor(sequence, idx)
-        if anchor_idx < 0:
+    def find(self, sequence: str) -> int:
+        if self.n_terminus and not sequence.startswith(self.n_terminus.amino):
             return -1
-        while anchor_idx >= 0:
+        if self.c_terminus and not sequence.endswith(self.c_terminus.amino):
+            return -1
+        anchor_idx = 0
+        while anchor_idx < len(sequence):
             matches = self.head.match_including_following(sequence, anchor_idx)
             if matches:
                 return anchor_idx
-            anchor_idx = self.find_anchor(sequence, anchor_idx + 1)
+            anchor_idx += 1
+            if self.n_terminus:
+                break
         return -1
 
-    def find_anchor(self, sequence: str, idx: int = 0) -> int:
-        length = len(sequence)
-        if idx < 0:
-            idx = length + idx
-        while idx < length:
-            if self.head.match(sequence[idx]):
-                return idx
-            idx += 1
-        return -1
 
-    def find_all(self, sequence: str) -> List[int]:  # TODO: is just find from offset, implement *real* findall
+    def find_all(self, sequence: str) -> List[MatchLocation]:
         results = []
-        match = self.find(sequence)
-        while match >= 0:
-            results.append(match)
-            match = self.find(sequence, match + 1)
+        index = 0
+        while index < len(sequence):
+            results.extend(self.head.match_all_possible(sequence, index))
+            index += 1
+            if self.n_terminus:
+                break
         return results
 
 
 def parse_repeats(sequence: str, offset: int) -> Tuple[int, int]:
-    #if not sequence[offset:]:
-    #    return 1, 1
     if (len(sequence) - 1) < offset:
         return 1, 1
     if sequence[offset] != "(" or not sequence.endswith(")"):
@@ -181,14 +281,20 @@ def parse_repeats(sequence: str, offset: int) -> Tuple[int, int]:
     return repeats[0], repeats[1]
 
 
-def parse_options(sequence: str, start: str, end: str) -> Tuple[Set[str], int, int]:
+def parse_options(sequence: str, start: str, end: str) -> Tuple[Set[str], int, int, bool]:  # TODO: solve that more nicely?
     if sequence[0] != start:
         raise ValueError("Brackets do not match")
     options = set()  # type: Set[str]
+    optional_c_terminus = False
     idx = 1
     while idx < len(sequence) and sequence[idx] != end:
         if sequence[idx] not in AMINOS:
-            raise ValueError("Invalid amino acid")
+            if sequence[idx] == ">" and not optional_c_terminus:
+                optional_c_terminus = True
+                idx += 1
+                continue
+            else:
+                raise ValueError("Invalid amino acid")
         options.add(sequence[idx])
         idx += 1
     if idx == len(sequence):
@@ -196,4 +302,4 @@ def parse_options(sequence: str, start: str, end: str) -> Tuple[Set[str], int, i
     min_repeats, max_repeats = parse_repeats(sequence, idx+1)
     if not options:
         raise ValueError("No valid options provided")
-    return options, min_repeats, max_repeats
+    return options, min_repeats, max_repeats, optional_c_terminus
--- antismash/common/test/test_pattern_search.py
+++ antismash/common/test/test_pattern_search.py
@@ -10,7 +10,6 @@ from antismash.common import pattern_search
 
 
 class TestParsePattern(unittest.TestCase):
-
     def test_simple_amino(self):
         simple_result = pattern_search.SimpleAmino('A')
         assert simple_result.amino == 'A'
@@ -61,37 +60,36 @@ class TestParsePattern(unittest.TestCase):
 
     def test_invalid_pattern(self):
         with self.assertRaises(ValueError):
-            pattern_search.Pattern('AA')
+            pattern_search.Pattern('AA.')
         with self.assertRaises(ValueError):
-            pattern_search.Pattern('*')
+            pattern_search.Pattern('*.')
 
     def test_find(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('Y')
+        pattern = pattern_search.Pattern('Y.')
         assert pattern.find(sequence) == -1
-
-        pattern = pattern_search.Pattern('A')
+        pattern = pattern_search.Pattern('A.')
         assert pattern.find(sequence) == 1
 
-        pattern = pattern_search.Pattern('A-G')
+        pattern = pattern_search.Pattern('A-G.')
         assert pattern.find(sequence) == 1
 
-        pattern = pattern_search.Pattern('A-Y')
+        pattern = pattern_search.Pattern('A-Y.')
         assert pattern.find(sequence) == -1
 
-        pattern = pattern_search.Pattern('A-T')
+        pattern = pattern_search.Pattern('A-T.')
         assert pattern.find(sequence) == 6
 
         assert pattern.find('') == -1
 
     def test_find_any(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('x')
+        pattern = pattern_search.Pattern('x.')
         assert pattern.find(sequence) == 0
 
     def test_find_single_then_any(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('M-x')
+        pattern = pattern_search.Pattern('M-x.')
         assert pattern.find('M') == -1
         assert pattern.find('') == -1
         assert pattern.find('AM') == -1
@@ -99,7 +97,7 @@ class TestParsePattern(unittest.TestCase):
 
     def test_find_any_combos(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('x-A')
+        pattern = pattern_search.Pattern('x-A.')
         assert pattern.find('A') == -1
         assert pattern.find('') == -1
         assert pattern.find('AC') == -1
@@ -107,20 +105,20 @@ class TestParsePattern(unittest.TestCase):
 
 
         # three-letter combinations:
-        pattern = pattern_search.Pattern('x-A-x')
+        pattern = pattern_search.Pattern('x-A-x.')
         assert pattern.find(sequence) == 0
         assert pattern.find('A') == -1
 
     def test_find_multiple(self):
         sequence = 'MAGICHAT'
         # simplest but inefficient: one acceptable amino acid
-        pattern = pattern_search.Pattern('[A]')
+        pattern = pattern_search.Pattern('[A].')
         assert pattern.find(sequence) == 1
         assert pattern.find('M') == -1
         assert pattern.find('') == -1
 
         # actual use case: multiple acceptable amino acids
-        pattern = pattern_search.Pattern('[AC]')
+        pattern = pattern_search.Pattern('[AC].')
         # This finds an A...
         assert pattern.find(sequence) == 1
         # ...or a C
@@ -129,12 +127,12 @@ class TestParsePattern(unittest.TestCase):
 
     def test_find_negated(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('{A}')
+        pattern = pattern_search.Pattern('{A}.')
         assert pattern.find(sequence) == 0
         assert pattern.find('A') == -1
         assert pattern.find('') == -1
 
-        pattern = pattern_search.Pattern('{MAGIC}')
+        pattern = pattern_search.Pattern('{MAGIC}.')
         assert pattern.find(sequence) == 5
         assert pattern.find('MAGIC') == -1
         assert pattern.find('GAMMA') == -1
@@ -142,51 +140,51 @@ class TestParsePattern(unittest.TestCase):
     def test_find_any_repeats(self):
         sequence = 'MAGICHAT'
 
-        pattern = pattern_search.Pattern('M-x-x-x-C')
+        pattern = pattern_search.Pattern('M-x-x-x-C.')
         assert pattern.find(sequence) == 0
 
-        pattern = pattern_search.Pattern('M-x(3)-C')
+        pattern = pattern_search.Pattern('M-x(3)-C.')
         assert pattern.find(sequence) == 0
 
     def test_find_multiple_repeats(self):
         sequence = 'MAGICHAT'
 
-        pattern = pattern_search.Pattern('[AT](2)')
+        pattern = pattern_search.Pattern('[AT](2).')
         assert pattern.find(sequence) == 6
         assert pattern.find('A') == -1
 
     def test_find_negated_repeats(self):
-        pattern = pattern_search.Pattern('{A}(3)')
+        pattern = pattern_search.Pattern('{A}(3).')
         assert pattern.find('BB') == -1
 
     def test_find_range(self):
         sequence = 'MYKITTYYYY'
 
-        pattern = pattern_search.Pattern('Y(1,2)')
+        pattern = pattern_search.Pattern('Y(1,2).')
         assert pattern.find(sequence) == 1
 
-        pattern = pattern_search.Pattern('Y(2,4)')
+        pattern = pattern_search.Pattern('Y(2,4).')
         assert pattern.find(sequence) == 6
 
-        pattern = pattern_search.Pattern('T(2,3)-Y')
+        pattern = pattern_search.Pattern('T(2,3)-Y.')
         assert pattern.find(sequence) == 4
 
-        pattern = pattern_search.Pattern('[TY](2,3)')
+        pattern = pattern_search.Pattern('[TY](2,3).')
         assert pattern.find(sequence) == 4
 
-        pattern = pattern_search.Pattern('{MY}(2,3)')
+        pattern = pattern_search.Pattern('{MY}(2,3).')
         assert pattern.find(sequence) == 2
 
-        pattern = pattern_search.Pattern('x(2,3)')
+        pattern = pattern_search.Pattern('x(2,3).')
         assert pattern.find(sequence) == 0
 
-        pattern = pattern_search.Pattern('T-Y(0,1)')
+        pattern = pattern_search.Pattern('T-Y(0,1).')
         assert pattern.find(sequence) == 4
 
-        pattern = pattern_search.Pattern('K-Y(0,1)-I')
+        pattern = pattern_search.Pattern('K-Y(0,1)-I.')
         assert pattern.find(sequence) == 2
 
-        pattern = pattern_search.Pattern('K-x(0,1)-I')
+        pattern = pattern_search.Pattern('K-x(0,1)-I.')
         assert pattern.find(sequence) == 2
 
     def test_parse_repeats(self):
@@ -213,46 +211,143 @@ class TestParsePattern(unittest.TestCase):
 
     def test_range_with_optional(self):
         sequence = 'MAGICHAT'
-        pattern = pattern_search.Pattern('H-A-T-S(0,1)')
+        pattern = pattern_search.Pattern('H-A-T-S(0,1).')
         assert pattern.find(sequence) == 5
 
-    def test_find_anchor(self):
+    def test_termini(self):
         sequence = 'MAGICHAT'
+        pattern = pattern_search.Pattern('<M.')
+        assert pattern.find(sequence) == 0
+        assert pattern.find('AMAGICHAT') == -1
 
-        pattern = pattern_search.Pattern('Y')
-        assert pattern.find_anchor(sequence) == -1
+        pattern = pattern_search.Pattern('T>.')
+        assert pattern.find(sequence) == 7
+        assert pattern.find('MAGICHATS') == -1
 
-        pattern = pattern_search.Pattern('A')
-        assert pattern.find_anchor(sequence) == 1
-        assert pattern.find_anchor(sequence, 2) == 6
-        assert pattern.find_anchor(sequence, 7) == -1
+        pattern = pattern_search.Pattern('<M-x(6)-T>.')
+        assert pattern.find(sequence) == 0
+        assert pattern.find('MAGICHATS') == -1
+        assert pattern.find('AMAGICHAT') == -1
 
-        # test out of bounds value
-        assert pattern.find_anchor(sequence, 12) == -1
+        sequence = 'MMMCHEESE'
+        pattern = pattern_search.Pattern('<M(3).')
+        assert pattern.find(sequence) == 0
 
-        # test searching from the end
-        assert pattern.find_anchor(sequence, -3) == 6
-        assert pattern.find_anchor(sequence, -1) == -1
+        pattern = pattern_search.Pattern('<M(3,4).')
+        assert pattern.find(sequence) == 0
 
-    def test_find_all(self):
-        sequence = 'HEYKITTYKITTY'
-        pattern = pattern_search.Pattern('Y')
-        assert pattern.find_all(sequence) == [2, 7, 12]
+        with self.assertRaisesRegex(ValueError, 'Invalid pattern'):
+            pattern_search.Pattern('A>-T.')
 
-        pattern = pattern_search.Pattern('K-I-T(2)-Y')
-        assert pattern.find_all(sequence) == [3, 8]
+        with self.assertRaisesRegex(ValueError, 'Invalid pattern'):
+            pattern_search.Pattern('A>-T>.')
 
-        sequence = 'CATINTHEHAT'
-        pattern = pattern_search.Pattern('x-A-T')
-        assert pattern.find_all(sequence) == [0, 8]
+        with self.assertRaisesRegex(ValueError, 'Invalid pattern'):
+            pattern_search.Pattern('<M-<A.')
 
-        pattern = pattern_search.Pattern('[CH]-A-T')
-        assert pattern.find_all(sequence) == [0, 8]
+        with self.assertRaisesRegex(ValueError, 'Invalid pattern'):
+            pattern_search.Pattern('M-<A.')
 
-        pattern = pattern_search.Pattern('{M}-A-T')
-        assert pattern.find_all(sequence) == [0, 8]
-        # TODO: figure out sequence for checking overlapping matches
+    def test_amino_or_cterminus_hit(self):
+        sequence = 'MAGICHAT'
+        pattern = pattern_search.Pattern('T-[T>].')
+        assert pattern.find(sequence) == 7
 
-        sequence = 'HEEEEY'
-        pattern = pattern_search.Pattern('E(1,4)-Y')
-        assert pattern.find_all(sequence) == [1, 2, 3, 4]
+    def test_amino_or_cterminus_miss(self):
+        pattern = pattern_search.Pattern('A-[M>].')
+        assert pattern.find('MAGICHAT') == -1
+        assert len(pattern.find_all('MAGICHAT')) == 0
+
+    def test_amino_or_cterminus_hit_multiple(self):
+        pattern = pattern_search.Pattern('A-[GT>].')
+        assert pattern.find('MAGICHAT') == 1
+        assert len(pattern.find_all('MAGICHAT')) == 2
+
+    def test_optional_cterminus_repeat(self):
+        pattern = pattern_search.Pattern('A-[T>](1,2).')
+        assert pattern.find('MAGICHAT') == 6
+        hits = pattern.find_all('MAGICHAT')
+        assert len(hits) == 1
+        assert hits[0].start == 6 and hits[0].end == 8
+        assert "MAGICHAT"[hits[0].start:hits[0].end] == "AT"
+
+        pattern = pattern_search.Pattern('[T>](1,2).')
+        assert pattern.find('MAGICHAT') == 7
+        assert len(pattern.find_all('MAGICHAT')) == 1
+
+        with self.assertRaises(ValueError):
+            pattern_search.Pattern('A>-[T>].')
+        with self.assertRaises(ValueError):
+            pattern_search.Pattern('A>-C-[T>].')
+        with self.assertRaises(ValueError):
+            pattern_search.Pattern('A>-C.')
+
+    def test_match_all_lengths_12(self):
+        sequence = 'MAAGGIC'
+        multiple_as = pattern_search.SimpleAmino('A(1,2)', pattern_search.SimpleAmino('G(1,2)'))
+        simple_m = pattern_search.SimpleAmino('M', multiple_as)
+        assert len(simple_m.match_all_possible(sequence)) == 2
+
+    def test_match_all_lengths_04(self):
+        sequence = 'MAAAAGIC'
+        multiple_as = pattern_search.SimpleAmino('A(0,4)')
+        simple_m = pattern_search.SimpleAmino('M', multiple_as)
+        assert len(simple_m.match_all_possible(sequence)) == 5
+
+    def test_match_all_lengths_03(self):
+        sequence = 'AAATAAAA'
+        multi_a_1 = pattern_search.SimpleAmino('A(0,3)')
+        simple_t = pattern_search.SimpleAmino('T', multi_a_1)
+        multi_a_2 = pattern_search.SimpleAmino('A(0,3)', simple_t)
+        assert len(multi_a_2.match_all_possible(sequence)) == 4
+
+    def test_leading_repeat(self):
+        sequence = 'MMAGIC'
+        result = pattern_search.Pattern('M(1,2)-A.').head.match_all_possible(sequence)
+        assert len(result) == 1
+
+        result = pattern_search.Pattern('M(1,2)-A.').find_all(sequence)
+        assert len(result) == 2
+
+    def test_leading_multiple_repeats(self):
+        sequence = 'MMAGIC'
+        result = pattern_search.Pattern('M(1,2)-A(0,1).').head.match_all_possible(sequence)
+        assert len(result) == 3
+
+        result = pattern_search.Pattern('M(1,2)-A(0,1).').find_all(sequence)
+        assert len(result) == 5
+
+    def test_find_all_multiple_repeats(self):
+        sequence = 'AAATAAAA'
+        pattern = pattern_search.Pattern('A(0,3)-T-A(0,3).')
+        assert len(pattern.find_all(sequence)) == 16
+
+    def test_find_all_start(self):
+        sequence = 'TAAATAAAATA'
+        pattern = pattern_search.Pattern('<T-A(0,3).')
+        assert len(pattern.find_all(sequence)) == 4
+
+    def test_find_all_end(self):
+        pattern = pattern_search.Pattern('A(0,3)-T-A>.')
+        assert len(pattern.find_all('TAAATAAAATA')) == 4
+
+    def test_find_all_basic_types(self):
+        sequence = 'MAGICHAT'
+        pattern = pattern_search.Pattern('x(3).')
+        assert len(pattern.find_all(sequence)) == 6
+
+        pattern = pattern_search.Pattern('[MH]-[AGI](0,3).')
+        assert len(pattern.find_all(sequence)) == 6
+
+        pattern = pattern_search.Pattern('{AG}(1,3).')
+        assert len(pattern.find_all(sequence)) == 8
+
+    def test_distance(self):
+        pattern = pattern_search.Pattern('M-A-G-I-C.')
+        result = pattern.find_all('MAGICHAT')
+        assert result[0].end == 5
+        assert result[0].length == 5
+
+        result = pattern.find_all('AMAGICHAT')
+        assert result[0].end == 6
+        assert result[0].length == 5
